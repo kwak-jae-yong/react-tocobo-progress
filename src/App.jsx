ead031a27d3b1e4e35cd8ce08dccabab63a0b9ee
kwak-jae-yong <dream001211@naver.com>
2025-04-11 12:24:41 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+
+import './App.scss'
+import Topbanner from './components/Topbanner'
+function App() {
+
+  return (
+    <div className='app-container'>
+      <Topbanner/>
+
+      <div className="inner">
+
+      <a href="" className="con_btn">보러가기</a>
+      <a href="" className="con_btn blue">보러가기</a>
+      <p className="txt">
+      토코보는 본질을 중시하며 직관을 바탕으로 한 제품을 선보입니다
+      </p>
+      <p className="txt  l_txt">
+      토코보는 본질을 중시하며 직관을 바탕으로 한 제품을 선보입니다
+      </p>
+      <p className="txt_1 ">
+      토코보는 본질을 중시하며 직관을 바탕으로 한 제품을 선보입니다
+      </p>
+      <p className="txt_2">
+      토코보는 본질을 중시하며 직관을 바탕으로 한 제품을 선보입니다
+      </p>
+      <p className="txt_3">
+      토코보는 본질을 중시하며 직관을 바탕으로 한 제품을 선보입니다
+      </p>
+      <p className="txt_4">
+      토코보는 본질을 중시하며 직관을 바탕으로 한 제품을 선보입니다
+      </p>
+      <p className="txt_5">
+      토코보는 본질을 중시하며 직관을 바탕으로 한 제품을 선보입니다
+      </p>
+      
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Topbanner.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './styles/Topbanner.scss'
+import {Swiper, SwiperSlide} from 'swiper/react'
+import {Autoplay} from 'swiper/modules'
+import 'swiper/css'
+const Topbanner = () => {
+  return (
+    <div className='top_banner'>
+        <Swiper
+            direction='vertical'
+            slidesPerView={1}
+            loop={true}
+            autoplay={{delay:3000}}
+            modules={[Autoplay]}
+            className='tb_slider'
+        >
+            <SwiperSlide>
+                <a href="#">새로운 소식이 있습니다!</a>
+            </SwiperSlide>
+            <SwiperSlide>
+                <a href="#">업데이트 안내</a>
+            </SwiperSlide>
+            <SwiperSlide>
+                <a href="#">이벤트 진행중</a>
+            </SwiperSlide>
+        </Swiper>
+    </div>
+  )
+}
+
+export default Topbanner
